cd7a87a97af509205e2dd2867fb883356baf747a
Denise Stark <starkwalker88@gmail.com>
2024-04-30 16:54:39 -0700

    getting started with quiz

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,10 @@
-import { log } from '../log.js';
-import logoImg from '../assets/logo.png';
+import logoImg from '../assets/quiz-logo.png';
 
 export default function Header() {
-  log('<Header /> rendered', 1);
-
   return (
-    <header id="main-header">
-      <img src={logoImg} alt="Magnifying glass analyzing a document" />
-      <h1>React - Behind The Scenes</h1>
+    <header>
+      <img src={logoImg} alt="Quiz logo" />
+      <h1>ReactQuiz</h1>
     </header>
   );
 }
